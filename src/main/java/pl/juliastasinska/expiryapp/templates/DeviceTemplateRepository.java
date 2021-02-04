04c152c7f7dc63748ca4b531f14a367b72f8534d
Julia Stasinska <julsta@gmail.com>
2021-02-04 14:53:08 +0100

    Templates, their repositories, controller and facade.

--- src/main/java/pl/juliastasinska/expiryapp/templates/DeviceTemplateRepository.java
+++ src/main/java/pl/juliastasinska/expiryapp/templates/DeviceTemplateRepository.java
@@ -1,6 +1,7 @@
 package pl.juliastasinska.expiryapp.templates;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.juliastasinska.expiryapp.templates.dto.DeviceTemplateDto;
 
 import java.util.List;
 import java.util.Optional;
